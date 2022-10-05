1cdf841542fdb7b1bbd8e830bf13e5f920cd8d02
trinh <trinh0792545710@gmail.com>
2022-10-05 21:46:53 +0700

    search

--- products/show.php
+++ products/show.php
@@ -11,7 +11,11 @@ $stmt = $pdo->prepare($sql);
 $stmt->bindParam(":id", $id);
 $stmt->execute();
 
-
+// view + 1
+$sql_update = "UPDATE products SET view = view + 1 WHERE id=:id";
+$stmt2 = $pdo->prepare($sql_update);
+$stmt2->bindParam(":id", $id);
+$stmt2->execute();
 
 $product = $stmt->fetch();
 
